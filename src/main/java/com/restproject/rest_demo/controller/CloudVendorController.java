aa8a1eb409c85a075cd971c0380d9866481d9e5c
roddalons <rodrigo.alo.mel@gmail.com>
2026-06-24 15:16:53 -0300

    Spring Boot Testing

---
+++ src/main/java/com/restproject/rest_demo/controller/CloudVendorController.java
@@ -0,0 +1,56 @@
+package com.restproject.rest_demo.controller;
+
+import com.restproject.rest_demo.model.CloudVendor;
+import com.restproject.rest_demo.service.CloudVendorService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cloudvendor")
+public class CloudVendorController
+{
+
+    CloudVendorService cloudVendorService;
+
+    public CloudVendorController(CloudVendorService cloudVendorService) {
+        this.cloudVendorService = cloudVendorService;
+    }
+
+    //Read Specific Cloud Vendor Details from DB
+    @GetMapping("{vendorId}")
+    public CloudVendor getCloudVendorDetails(@PathVariable("vendorId") String vendorId)
+    {
+        return cloudVendorService.getCloudVendor(vendorId);
+    }
+    //Read All Cloud Vendors from DB
+    @GetMapping()
+    public List<CloudVendor> getAllCloudVendorDetails()
+    {
+        return cloudVendorService.getAllCloudVendors();
+    }
+
+
+    @PostMapping
+    public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor)
+    {
+        cloudVendorService.createCloudVendor(cloudVendor);
+        return "Cloud Vendor Created Succesfully";
+    }
+
+
+    @PutMapping
+    public String updateCloudVendorDetails(@RequestBody CloudVendor cloudVendor)
+    {
+        cloudVendorService.updateCloudVendor(cloudVendor);
+        return "Cloud Vendor Updated Succesfully";
+    }
+
+
+    @DeleteMapping("{vendorId}")
+    public String deleteCloudVendorDetails(@PathVariable("vendorId") String vendorId)
+    {
+        cloudVendorService.deleteCloudVendor(vendorId);
+        return "Cloud Vendor Deleted Succesfully";
+    }
+}
---
+++ src/main/java/com/restproject/rest_demo/repository/CloudVendorRepository.java
@@ -0,0 +1,7 @@
+package com.restproject.rest_demo.repository;
+
+import com.restproject.rest_demo.model.CloudVendor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CloudVendorRepository extends JpaRepository<CloudVendor, String> {
+}
